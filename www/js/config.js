3ef80ea78584b628d1e7e04c9333f42ba8791e1d
carterax <ikenna750@gmail.com>
2018-01-31 00:16:02 +0100

    initial commit

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+
+var krms_config ={					
+    'ApiUrl':"http://home.skyearning.com/merchantapp/api",	           
+	'DialogDefaultTitle':"zoulak merchant app",
+	'pushNotificationSenderid':"600405925358",
+	'APIHasKey':""
+};
