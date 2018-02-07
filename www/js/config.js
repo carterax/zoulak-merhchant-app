f40503712b5191b0e6e775f334d4805c5d7cf1e7
carterax <ikenna750@gmail.com>
2018-02-07 02:24:14 +0100

    res files for platforms

--- www/js/config.js
+++ www/js/config.js
@@ -1,6 +1,6 @@
 
 var krms_config ={					
-    'ApiUrl':"http://home.skyearning.com/merchantapp/api",	           
+    'ApiUrl':"http://zoulak.com/merchantapp/api",	           
 	'DialogDefaultTitle':"zoulak merchant app",
 	'pushNotificationSenderid':"600405925358",
 	'APIHasKey':""
